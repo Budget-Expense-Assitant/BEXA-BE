991a47755bf3a7b3124e3b15aa4e0aa2788d535c
Marlin Rok <Marlin.Rok@datev.de>
2026-01-19 14:54:29 +0100

    feat: add user error messages and handle user existence check in UserService

---
+++ src/main/java/de/bexa/errorMessages/UserErrorMessages.java
@@ -0,0 +1,5 @@
+package de.bexa.errorMessages;
+
+public class UserErrorMessages {
+    public static final String USER_ALREADY_EXISTS = "Benutzer existiert bereits";
+}
--- src/main/java/de/bexa/user/control/UserService.java
+++ src/main/java/de/bexa/user/control/UserService.java
@@ -1,10 +1,13 @@
 package de.bexa.user.control;
 
+import de.bexa.errorMessages.UserErrorMessages;
 import de.bexa.repository.UserRepository;
 import de.bexa.user.boundary.dto.UserCreationRequest;
 import de.bexa.user.entity.User;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.Date;
 
@@ -14,6 +17,10 @@ public class UserService {
     private final UserRepository userRepository;
 
     public User createUser(UserCreationRequest userCreationRequest) {
+        if (userRepository.findAll().stream().anyMatch(user -> user.getUserName().equalsIgnoreCase(userCreationRequest.getUserName()))) {
+            throw new ResponseStatusException(HttpStatus.CONFLICT, UserErrorMessages.USER_ALREADY_EXISTS);
+        }
+
         User user = User.builder()
                 .userName(userCreationRequest.getUserName())
                 .password(userCreationRequest.getPassword())
