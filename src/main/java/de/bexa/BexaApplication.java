dfe76ef67e128df4ab2bc3ba278aada89ce27e19
Marlin Rok <Marlin.Rok@datev.de>
2026-01-19 10:13:28 +0100

    initial commit

---
+++ src/main/java/de/bexa/BexaApplication.java
@@ -0,0 +1,13 @@
+package de.bexa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BexaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BexaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/bexa/BexaApplicationTests.java
@@ -0,0 +1,13 @@
+package de.bexa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BexaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
