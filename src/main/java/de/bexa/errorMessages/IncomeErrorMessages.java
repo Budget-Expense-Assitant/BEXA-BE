3bf5629e6819e51ee6b6a71f269afc91f9abbaab
Ioannis Pantsourakis <pantsourakisi@gmail.com>
2026-01-21 12:00:19 +0100

    Fehlermeldungen erstellt

---
+++ src/main/java/de/bexa/errorMessages/IncomeErrorMessages.java
@@ -0,0 +1,9 @@
+package de.bexa.errorMessages;
+
+public class IncomeErrorMessages {
+    public static final String INCOME_AMOUNT_NOT_GIVEN = "Höhe der Einnahme nicht gegeben!";
+    public static final String INCOME_DATE_NOT_GIVEN = "Eingangsdatum der Einnahme nicht gegeben!";
+    public static final String INCOME_SOURCE_NOT_GIVEN = "Quelle der Einnahme nicht gegeben!";
+    public static final String INCOME_RECURRING_STATE = "Sie müssen angeben, ob die Einnahme wiederkehrbar ist!";
+    public static final String INCOME_DESCRIPTION = "Sie müssen eine Beschreibung der Einnahme angeben!";
+}
